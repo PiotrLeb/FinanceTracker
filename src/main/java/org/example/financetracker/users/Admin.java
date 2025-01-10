acd1f31c0b8549fbc7799dc3d6c5a28a7adda141
Piotr <piotr.lebioda14@gmail.com>
2025-01-11 00:55:18 +0100

    javadoc documentation

--- src/main/java/org/example/financetracker/users/Admin.java
+++ src/main/java/org/example/financetracker/users/Admin.java
@@ -1,18 +1,36 @@
 package org.example.financetracker.users;
 
 public class Admin extends User {
+    /**
+     *
+     * @param username
+     * @param password
+     * @param email
+     */
     public Admin(String username, String password, String email) {
         super(username, password, email);
     }
 
+    /**
+     *
+     * @return
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getEmail() {
         return email;
     }
