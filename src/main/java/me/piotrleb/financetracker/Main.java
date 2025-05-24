fb44e3df72ab9364d2c946cb7b07d66bc6790976
Piotr <piotr.lebioda14@gmail.com>
2025-05-24 22:15:18 +0200

    Added register window

--- src/main/java/me/piotrleb/financetracker/Main.java
+++ src/main/java/me/piotrleb/financetracker/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("views/login.fxml")));
         Scene scene = new Scene(root, 1500, 1000);
         Font.loadFont(Objects.requireNonNull(getClass().getResource("/me/piotrleb/financetracker/fonts/LexendDeca-Regular.ttf")).toExternalForm(), 10);
-        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("css/styles.css")).toExternalForm());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("css/main.css")).toExternalForm());
         stage.setTitle("Finance Tracker");
         stage.setScene(scene);
         stage.show();
@@ -27,7 +27,10 @@ public class Main extends Application {
 
     public static void setRoot(String fxml) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource(fxml)));
-        stage.setScene(new Scene(root, 1500, 1000));
+        Scene scene = new Scene(root, 1500, 1000);
+        Font.loadFont(Objects.requireNonNull(Main.class.getResource("/me/piotrleb/financetracker/fonts/LexendDeca-Regular.ttf")).toExternalForm(), 10);
+        scene.getStylesheets().add(Objects.requireNonNull(Main.class.getResource("css/main.css")).toExternalForm());
+        stage.setScene(scene);
     }
 
     public static void main(String[] args) {
--- src/main/java/me/piotrleb/financetracker/controllers/RegisterController.java
+++ src/main/java/me/piotrleb/financetracker/controllers/RegisterController.java
@@ -8,12 +8,22 @@ import me.piotrleb.financetracker.Main;
 import me.piotrleb.financetracker.users.User;
 import me.piotrleb.financetracker.utils.UserPasser;
 
-public class LoginController {
 
+public class RegisterController {
     @FXML
     private TextField textField;
     @FXML
     private PasswordField passwordField;
+    @FXML
+    private PasswordField repeatPasswordField;
+
+    private void usernameTaken() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText("Username is already taken");
+        alert.showAndWait();
+    }
 
     private void fillForm(){
         Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -23,31 +33,27 @@ public class LoginController {
         alert.showAndWait();
     }
 
-    private void invalidData(){
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle("Error");
-        alert.setHeaderText(null);
-        alert.setContentText("Invalid username or password");
-        alert.showAndWait();
-    }
-
     @FXML
-    protected void signIn() throws Exception {
+    protected void register() throws Exception {
         String username = textField.getText();
         String password = passwordField.getText();
+        String repeatPassword = repeatPasswordField.getText();
 
         if(username.isEmpty() || password.isEmpty()){
             fillForm();
             return;
         }
-        if(!User.isUserExist(username, password)){
-            invalidData();
+
+        if(User.isUsernameExist(username)){
+            usernameTaken();
             return;
         }
-
-        User user = new User(username, password);
-        UserPasser.setInstance(user);
-        Main.setRoot("views/main.fxml");
+        if(password.equals(repeatPassword)){
+            User.insertUser(username, password);
+            User user = new User(username, password);
+            UserPasser.setInstance(user);
+            Main.setRoot("views/main.fxml");
+        }
     }
 
 }
